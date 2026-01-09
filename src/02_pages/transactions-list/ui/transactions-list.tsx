348ba7c2fc5875a143ca16a608fb760f5aa3038e
DmitriyOne <senkoner4@gmail.com>
2026-01-09 15:59:02 +0000

    Fake data for transactions

--- src/02_pages/transactions-list/ui/transactions-list.tsx
+++ src/02_pages/transactions-list/ui/transactions-list.tsx
@@ -1,4 +1,11 @@
+import { useLoaderData } from "react-router"
+import type { TTransactionListResponse } from "@/05_entities/transaction"
+
 export const TransactionsListPage = () => {
+  const data = useLoaderData() as TTransactionListResponse
+
+  console.log(data)
+
   return (
     <div className='container mx-auto px-4 py-8'>
       <h1 className='text-3xl font-bold text-gray-900'>Transactions</h1>
